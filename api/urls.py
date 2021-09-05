2271afaa9f09b22d6675fed56a939e0081a962d5
Daria <hwda@yandex.ru>
2021-09-05 20:18:32 +0300

    test2

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,8 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import (
-    APIGetToken, APINewUser, CategoryViewSet, CommentViewSet, GenreViewSet,
-    ReviewViewSet, TitleViewSet, UserViewSet,
-)
+from .views import (APIGetToken, APINewUser, CategoryViewSet, CommentViewSet,
+                    GenreViewSet, ReviewViewSet, TitleViewSet, UserViewSet)
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='users')
