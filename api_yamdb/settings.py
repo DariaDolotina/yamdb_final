498d2d38adc16d8950e0e8bcb82578e8b7bd1b9d
Daria <hwda@yandex.ru>
2021-09-07 22:33:58 +0300

    test

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='p&l%385148kslhtyn^##a1)ilz@4zqj=rq
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['130.193.38.233']
+ALLOWED_HOSTS = ['130.193.38.233', 'localhost', '127.0.0.1']
 
 
 INSTALLED_APPS = [
