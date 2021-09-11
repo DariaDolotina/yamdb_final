40a749d07942bcf3bb4692e0df6e4fa708292f4b
Daria <hwda@yandex.ru>
2021-09-11 15:07:07 +0300

    update settings

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -61,7 +61,7 @@ DATABASES = {
         'ENGINE': os.environ.get('DB_ENGINE', default='django.db.backends.postgresql'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT'),
     }
